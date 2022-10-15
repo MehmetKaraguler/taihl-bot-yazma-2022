d8cf41ba03af6a291f0bc1e2d9d047056664eb83
MehmetKaraguler <114801986+MehmetKaraguler@users.noreply.github.com>
2022-10-15 12:04:22 +0300

    Create string_formatlama.py

---
+++ string_formatlama.py
@@ -0,0 +1,13 @@
+# Kullanıcıdan isim, soyad ve yaş bilgilerini alalım
+isim=input("İsminiz: ")
+soyad=input("Soyadınız:")
+yas=input("Yaşınız:")
+
+# Kullanıcı bilgilerini yazdıralım
+#  Adınız:Mehmet Karagüler,Yaşınız:14
+print("Adınız: " + isim + " " + soyad + ", Yaşınız: " + yas)
+print("Adınız: {} {}, Yaşınız: {}".format(isim, soyad, yas))
+print("Adınız: {2} {0}, Yaşınız: {1}".format(soyad, yas, isim))
+print("Adınız: {x} {y}, Yaşınız: {z}".format(y=soyad, z=yas, x=isim))
+print(f"Adınız: {isim} {soyad}, Yaşınız: {yas}")
+
