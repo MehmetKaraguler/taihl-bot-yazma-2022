6205ae4de30054ad364cfa03dab986290e474a45
MehmetKaraguler <karagulermehmet@gmail.com>
2022-11-26 11:05:28 +0300

    atama operatorleri

--- atama_operatorleri.py
+++ atama_operatorleri.py
@@ -22,3 +22,37 @@ Python'da değişkenlere veri ataması yaparken kullanırız.
 | //=      | x //= y   | x = x // y     |
 +----------+-----------+----------------+
 """
+
+# sayısal verilere sahip 3 değişken oluşturalım
+a = 2
+b = 3
+c = 4
+
+
+# üç değikeni tek satırda tanımlayalım
+x, y, z =5,6,7
+print(x,y,z)
+
+# değikendeki verileri yer değiştirelim
+x, y, z = y, z, x
+print(x, y, z)
+
+# değiken içindeki değere 10 ekleyelim
+x = x + 10
+y+= 10
+print(x, y)
+# x ve y değişkenlerinde değeri 5 azaltalım (x: uzun yol, y: kısa yol)
+x =x - 5
+y-=5
+print(x,y)
+
+
+
+
+
+
+
+
+
+
+
