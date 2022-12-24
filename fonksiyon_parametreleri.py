ab10719c783b5c24315e336ce4e0200cc3f7ea0f
MehmetKaraguler <karagulermehmet@gmail.com>
2022-12-24 09:28:28 +0300

    Update fonksiyon_parametreleri.py

--- fonksiyon_parametreleri.py
+++ fonksiyon_parametreleri.py
@@ -12,6 +12,17 @@ def ogrenci_kaydet(ad, soyad, notlar):
     dosya.write(f"Ad: {ad}, Soyad: {soyad}, Notlar: {notlar}, Ortalama: {ortalama}\n")
     dosya.close()
 
+def ogrenci_kaydet2(ad , soyad , notlar=None)
+    if notlar:  # if isinstance(notlar,str)
+        # notları integer yap ve ortalamayı hesapla
+notlar_list =) [int(x) for x in notlart.split(",")]
+ortalama = sum /notlar_list/ len (notlar_list)
+else
+notlar=  "not bilgisi yok "
+ortalama  = "ortalama hesaplanamadı"
+
+ dosya = open("öğrenciler.txt,mode=a,encoding="utf-8"
+
 
 while True:
     ne_yapilacak = input("Ne yapacaksınız? (1: Kaydet, 0: Çık): ")
